53a6af57bf9735bf6ccfcc29609607c0eb86a06f
ugur.hicyilmam <ugurhicyilmam@gmail.com>
2016-10-06 15:05:20 +0300

    Adds test profile to test

--- bwat-server/src/test/java/com/bwat/BwatServerApplicationTests.java
+++ bwat-server/src/test/java/com/bwat/BwatServerApplicationTests.java
@@ -3,10 +3,13 @@ package com.bwat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class BwatServerApplicationTests {
 
 	@Test
