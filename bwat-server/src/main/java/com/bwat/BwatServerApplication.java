082d5550aef5daa386fa6be29a03e41c551f8494
ugur.hicyilmam <ugurhicyilmam@gmail.com>
2016-10-06 15:05:03 +0300

    Removes unnecessary configuration

--- bwat-server/src/main/java/com/bwat/BwatServerApplication.java
+++ bwat-server/src/main/java/com/bwat/BwatServerApplication.java
@@ -1,21 +1,11 @@
 package com.bwat;
 
-import com.bwat.config.DataLoader;
-import com.bwat.core.repository.IAccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BwatServerApplication {
-
-    @Autowired
-    IAccountRepository accountRepository;
-
-    @Autowired
-    DataLoader dataLoader;
-
-	public static void main(String[] args) {
-		SpringApplication.run(BwatServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BwatServerApplication.class, args);
+    }
 }
