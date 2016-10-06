b91067234f4ff26634814d7673c16f1f7d092189
ugur.hicyilmam <ugurhicyilmam@gmail.com>
2016-10-06 15:04:41 +0300

    #39 Adds service layer for registration endpoint

---
+++ bwat-server/src/main/java/com/bwat/core/service/impl/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package com.bwat.core.service.impl;
+
+import com.bwat.core.domain.User;
+import com.bwat.core.repository.UserRepository;
+import com.bwat.core.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+/**
+ * Created by ugur.hicyilmam on 6.10.2016.
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User register(User user) {
+        user.setRegistered(new Date());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public User findByMail(String email) {
+        return userRepository.findByMail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return findByMail(s);
+    }
+}
