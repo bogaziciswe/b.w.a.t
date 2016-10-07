7fb3014389af83fd3ec04c457cb7398ad2ecbc21
ugurhicyilmam <ugurhicyilmam@gmail.com>
2016-10-07 22:17:25 +0300

    #43 Adds endpoints for authentication

--- bwat-server/src/main/java/com/bwat/controller/UserController.java
+++ bwat-server/src/main/java/com/bwat/controller/UserController.java
@@ -3,6 +3,9 @@ package com.bwat.controller;
 import com.bwat.core.domain.User;
 import com.bwat.core.request.RegisterReq;
 import com.bwat.core.service.UserService;
+import com.bwat.exceptions.AuthenticationFailed;
+import com.bwat.transfer.SuccessMessage;
+import com.bwat.transfer.SuccessResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -24,17 +27,30 @@ public class UserController {
     @Autowired
     private ModelMapper modelMapper;
 
-    @PreAuthorize("isAuthenticated()")
-    @RequestMapping(value = "/user")
-    public Principal user(Principal user) {
-        return user;
+    @RequestMapping(value = "/login")
+    public SuccessMessage login(Principal user) {
+        if(user == null) {
+            throw new AuthenticationFailed();
+        }
+        return new SuccessResponse(userService.findByMail(user.getName()));
     }
 
-    @PreAuthorize("isAnonymous()")
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public User create(@RequestBody @Valid RegisterReq req) {
+    public SuccessResponse create(@RequestBody @Valid RegisterReq req) {
         User user = new User();
         modelMapper.map(req, user);
-        return userService.register(user);
+        return new SuccessResponse(userService.register(user));
+    }
+
+    //todo remove this in production, it is here for testing
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/restricted", method = RequestMethod.GET)
+    public void restricted() {
+
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public SuccessMessage logout() {
+        return new SuccessResponse("Hint: The server works stateless. To logout, remove authentication header on client.");
     }
 }
