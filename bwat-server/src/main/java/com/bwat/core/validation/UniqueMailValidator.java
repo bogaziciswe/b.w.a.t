c6a23c4f06fd8f0c6954e7db821ffc9880ad02da
ugurhicyilmam <ugurhicyilmam@gmail.com>
2016-10-07 21:44:42 +0300

    #39 Adds validator for email uniqueness

---
+++ bwat-server/src/main/java/com/bwat/core/validation/UniqueMailValidator.java
@@ -0,0 +1,21 @@
+package com.bwat.core.validation;
+
+import com.bwat.core.domain.User;
+import com.bwat.core.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class UniqueMailValidator implements ConstraintValidator<UniqueMail, String> {
+
+    @Autowired
+    private UserService userService;
+
+    public void initialize(UniqueMail constraint) {
+    }
+
+    public boolean isValid(String mail, ConstraintValidatorContext context) {
+        return (userService.findByMail(mail) == null);
+    }
+}
