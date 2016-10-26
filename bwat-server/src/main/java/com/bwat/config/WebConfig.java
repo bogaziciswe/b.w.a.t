ff1f395fb4e04a7f7afe3f582db0947bcc74d948
ugur.hicyilmam <ugurhicyilmam@gmail.com>
2016-10-26 16:34:06 +0300

    #56 Adds necessary CORS configuration

---
+++ bwat-server/src/main/java/com/bwat/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.bwat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        super.addCorsMappings(registry);
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
