17d7a1b5a2cb7db05906aa76d0e467400873e36b
Gokhan Ozgozen <gokhan.ozgozen@gmail.com>
2016-10-28 17:18:07 +0300

    Annotation Store and Retrieve

--- browserExtension/content_scripts/annotationMain.js
+++ browserExtension/content_scripts/annotationMain.js
@@ -3,6 +3,15 @@ var protocol = "http://";
 var serverRootUrl = "192.168.0.103";
 var loginPostUri = ":8080/healthTracker/auth";
 var registerPostUri = ":8080/healthTracker//registerAuth";
+var annotationStorePostUri = ":8080/healthTracker";
+
+/**
+ * Hardcoded annotation store options
+ * create - /annotations -
+ *
+ *
+ * @author Gokhanozg
+ */
 
 var userAuthToken = "DEFAULT"; // leave this as DEFAULT
 
@@ -159,38 +168,20 @@ function startAnnotationDrawing() {
     });
 }
 function startAnnotatorJS() {
-    //var app = new annotator.App();
-    //app.include(annotator.ui.main);
-    //app.include(annotator.storage.http);
-    //app.start();
-    //if (app != null) {
-    //    alert("Please select something and click on annotate Icon");
-    //} else {
-    //    alert("Something is wrong with annotation selector");
-    //}
+    var app = new annotator.App();
+    app.include(annotator.ui.main);
+    app.include(annotator.storage.http, {
+        prefix: protocol + serverRootUrl + annotationStorePostUri
+    });
+    app.start().then(function () {
+        app.annotations.load();
+    });
+    if (app != null) {
+        alert("Please select something and click on annotate Icon");
+    } else {
+        alert("Something is wrong with annotation selector");
+    }
 
-    //var username = "gokhan";
-    //var password = "bwat";
-    //
-
-    //
-    //$.ajax
-    //({
-    //    type: "GET",
-    //    url: "http://192.168.0.103:8080/healthTracker/auth",
-    //    dataType: 'json',
-    //    async: false,
-    //    data: '{}',
-    //    beforeSend: function (xhr) {
-    //        xhr.setRequestHeader('Authorization', make_base_auth(username, password));
-    //    },
-    //    success: function () {
-    //        alert('Thanks for your comment!');
-    //    },
-    //    error: function (err) {
-    //        alert("La hilla heeeyy" + err.message + " , " + err);
-    //    }
-    //});
 }
 function startAnnotation(data) {
 
