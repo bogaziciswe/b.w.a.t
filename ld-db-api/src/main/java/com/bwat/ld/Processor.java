7eb3913f6468b3e56936fb3c52ab67a31a89ffff
skynyrd <anil.surmeli@gmail.com>
2016-12-04 19:01:38 +0300

    adds validator unit tests

--- ld-db-api/src/main/java/com/bwat/ld/Processor.java
+++ ld-db-api/src/main/java/com/bwat/ld/Processor.java
@@ -5,5 +5,5 @@ import com.github.jsonldjava.core.JsonLdError;
 import java.util.LinkedHashMap;
 
 public interface Processor {
-    public LinkedHashMap extractAnnotationModel(Object rawLD) throws JsonLdError;
+    LinkedHashMap extractAnnotationModel(Object rawLD) throws JsonLdError;
 }
