9092b4ab2cf4823136f321868a3622e88ab6fa45
Anıl Selim Sürmeli <Anl Selim Srmeli>
2016-12-17 13:44:59 +0300

    #000 anil adds id, type and format fields to target object

--- bwat-server/src/main/java/com/bwat/transfer/Target.java
+++ bwat-server/src/main/java/com/bwat/transfer/Target.java
@@ -5,5 +5,8 @@ import lombok.Data;
 @Data
 public class Target {
     private String source;
+    private String id;
+    private String format;
+    private String type;
     private Object selector;
 }
